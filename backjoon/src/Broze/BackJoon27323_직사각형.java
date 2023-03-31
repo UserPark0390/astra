e4b22c0e8456323e3f6b1cbcddc8e7ac5fc9d7f7
UserPark0390 <seungmin0390@gmail.com>
2023-04-01 01:29:57 +0900

    230401_dawn

---
+++ backjoon/src/Broze/BackJoon27323_직사각형.java
@@ -0,0 +1,15 @@
+package Broze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BackJoon27323_직사각형 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int A = Integer.parseInt(br.readLine());
+		int B = Integer.parseInt(br.readLine());
+		
+		System.out.println(A * B);
+	}
+}
--- backjoon/src/Broze/BackJoon2750_수정렬하기2.java
+++ backjoon/src/Broze/BackJoon2750_수정렬하기2.java
@@ -1,5 +1,30 @@
 package Broze;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class BackJoon2750_수정렬하기2 {
-		// Merge sort로 풀기 -> 
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int A [] = new int [N];
+		
+		for(int i = 0; i < N; i++) {
+			A[i] = Integer.parseInt(br.readLine());
+		}
+		
+		for(int i = 0; i < N-1; i++) {
+			for(int j = 0; j < N-1-i; j++) {
+				if(A[j] > A[j+1]) {
+					int tmp = A[j];
+					A[j] = A[j+1];
+					A[j+1] = tmp;
+				}
+			}
+		}
+		for(int i = 0; i < N; i++) {
+			System.out.println(A[i]);
+		}
+	}
 }
