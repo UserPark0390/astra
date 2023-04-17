ce42252589d01f587ca64f7705b78ab6a6c0158a
UserPark0390 <seungmin0390@gmail.com>
2023-04-17 23:19:34 +0900

    230417_night

---
+++ Programmers/src/level0/Programmers_순서쌍의개수.java
@@ -0,0 +1,17 @@
+package level0;
+
+public class Programmers_순서쌍의개수 {
+
+	public static void main(String[] args) {
+		
+	}
+	
+    public static int solution(int n) {
+        int answer = 0;
+        
+        
+        
+        return answer;
+    }
+	
+}
