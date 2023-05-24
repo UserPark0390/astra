ffcdefc65e58d1017e5d8a13caa919179bffb110
UserPark0390 <seungmin0390@gmail.com>
2023-05-24 22:26:43 +0900

    230524_night

--- Programmers/src/level0/solution/Programmers_치킨쿠폰.java
+++ Programmers/src/level0/solution/Programmers_치킨쿠폰.java
@@ -2,38 +2,20 @@ package level0.solution;
 
 public class Programmers_치킨쿠폰 {
 	public static void main(String[] args) {
-		int chicken = 1081;
+		int chicken = 100;
 		System.out.println(solution(chicken));
 	}
 
 	public static int solution(int chicken) {
 		int answer = 0;
-		int tmp = 0;
-		while (chicken >= 1) {
-			if (chicken / 10 >= 1 && chicken % 10 == 0) {
-				answer += chicken / 10;
-				chicken /= 10;
-				if (chicken < 10 && tmp > 0) {
-					chicken += tmp;
-					tmp = 0;
-				} else if(chicken >= 10) {
-					continue;
-				} else {
-					break;
-				}
-			} else if (chicken / 10 >= 1 && chicken % 10 > 0) {
-				answer += chicken / 10;
-				tmp += chicken % 10;
-				chicken = chicken / 10;
-				if (tmp >= 10) {
-					chicken += tmp / 10;
-					tmp = tmp % 10;
-				} else if(chicken >= 10){
-					continue;
-				} else {
-					break;
-				}
-			}
+		int coupon = chicken; // 쿠폰
+		while (coupon > 9) {
+			if (coupon / 10 >= 1) {
+				int change = coupon / 10;
+				answer += change;
+				coupon -= change * 10;
+				coupon += change;				
+			}		
 		}
 		return answer;
 	}
