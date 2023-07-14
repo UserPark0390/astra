09a54480e84fa0757e41afbc10671cf92a673b9b
UserPark0390 <seungmin0390@gmail.com>
2023-07-14 23:50:48 +0900

    230714_night

--- Programmers/src/level1/level1_solution/Programmers_키패드누르기.java
+++ Programmers/src/level1/level1_solution/Programmers_키패드누르기.java
@@ -10,12 +10,21 @@ public class Programmers_키패드누르기 {
     public static String solution(int[] numbers, String hand) {
         String answer = "";
         boolean hand_check = false; // 왼손을 false
+        boolean [][] number_check = {{false,false,false}, {false,false,false},{false,false,false},{true,false,true}};
         if(hand.equals("right")) {
         	hand_check = true; // 오른손을 true
         }
         String [][] phone = {{"1","2","3"}, {"4","5","6"},{"7","8","9"},{"*","0","#"}};
+        for(int i = 0; i < numbers.length; i++) {
+        	String str = String.valueOf(numbers[i]);
+        	switch(str) {
+        		case "1" :
+        			
+        	
+        	
+        	}
+        }
         
-        System.out.println();
         
         return answer;
     }
