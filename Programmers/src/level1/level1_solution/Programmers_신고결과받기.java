64c551642ff6c46b15c9ef0279297b2f5e6e49c8
UserPark0390 <seungmin0390@gmail.com>
2023-12-27 22:55:57 +0900

    231227_night(test)

---
+++ Programmers/src/level1/level1_solution/Programmers_신고결과받기.java
@@ -0,0 +1,15 @@
+package level1.level1_solution;
+
+public class Programmers_신고결과받기 {
+	public static void main(String[] args) {
+		String[] id_list = {"muzi", "frodo", "apeach", "neo"};
+		String[] report  = {"muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"};
+		int k = 2;
+		System.out.println(solution(id_list, report, k));
+	}
+	
+    public static int[] solution(String[] id_list, String[] report, int k) {
+        int[] answer = {};
+        return answer;
+    }
+}
